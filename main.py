18604b1efbb211caa085ca44a7edb02faf10f816
김효원 <kanna28051@gmail.com>
2025-06-09 20:52:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+import streamlit as st
+st.title('방명록!')
+name = st.text_input('이름을 입력하세요 : ')
+menu = st.selectbox('신분 :', ['학생', '선생님'])
+if st.button('인사말 생성'):
+  st.write(name + '님! 당신은 ' + menu + '이시군요 반갑습니다')
